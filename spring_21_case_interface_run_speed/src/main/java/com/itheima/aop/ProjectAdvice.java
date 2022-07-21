4c40006d9f9faca89a7adcb7ad842cf68981cded
Zr <z13926857059@icloud.com>
2022-07-21 10:38:45 +0800

    曾睿

--- spring_21_case_interface_run_speed/src/main/java/com/itheima/aop/ProjectAdvice.java
+++ spring_21_case_interface_run_speed/src/main/java/com/itheima/aop/ProjectAdvice.java
@@ -10,24 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class ProjectAdvice {
-    //匹配业务层的所有方法
+    //匹配业务层所有方法
     @Pointcut("execution(* com.itheima.service.*Service.*(..))")
-    private void servicePt(){}
+    private void servicePt() {}
 
-    //设置环绕通知，在原始操作的运行前后记录执行时间
     @Around("ProjectAdvice.servicePt()")
-    public void runSpeed(ProceedingJoinPoint pjp) throws Throwable {
-        //获取执行的签名对象
+    public void runSpeed(ProceedingJoinPoint pjp) throws Throwable{
         Signature signature = pjp.getSignature();
         String className = signature.getDeclaringTypeName();
         String methodName = signature.getName();
-
         long start = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
-           pjp.proceed();
+            pjp.proceed();
         }
         long end = System.currentTimeMillis();
-        System.out.println("万次执行："+ className+"."+methodName+"---->" +(end-start) + "ms");
+        System.out.println("万次执行: "+className+"."+methodName+"---->"+(end-start)+"ms");
     }
 
 }
