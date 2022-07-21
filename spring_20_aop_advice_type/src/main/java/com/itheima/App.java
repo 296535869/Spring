eeb8f657ccf7f2118bbcc479bad38ba5560474ed
Zr <z13926857059@icloud.com>
2022-07-21 10:07:32 +0800

    曾睿

--- spring_20_aop_advice_type/src/main/java/com/itheima/App.java
+++ spring_20_aop_advice_type/src/main/java/com/itheima/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
         BookDao bookDao = ctx.getBean(BookDao.class);
-        int num = bookDao.select();
-        System.out.println(num);
+        int select = bookDao.select();
+        System.out.println(select);
     }
 }
